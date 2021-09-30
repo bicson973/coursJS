226376fe5709238ff88757fe7c506dd7a542385b
Bicson STG <bicson.saint-germain@colombbus.org>
2021-09-30 16:45:08 +0200

    suite js

---
+++ 02_variables_constantes/02_04_operateur.js
@@ -0,0 +1,54 @@
+// FEUILLE JS - 04 OPERATEUR - SEMA 
+
+let x = 200;
+let y = 4; 
+let z = 40;
+console.log(x,y,z);
+
+let a = x + 1; 
+let b = x + y;
+let c = x - y;
+let d = x * y;
+let e = x / y;
+console.log(a,b,c,d,e);
+
+let f = 5 % 3; // le modulo = %
+console.log(f);
+
+let g = 2 ** 3; // exponentielle ou 2 puissance 3
+console.log(g);
+
+// Une alerte pour afficher les résultats. \n permet d'aller a la ligne.
+// alert('a contient : ' + a + 
+// '\n b contient : ' + b +
+// '\n b contient : ' + c +
+// '\n b contient : ' + d +
+// '\n b contient : ' + e 
+// );
+
+// priorité des calculs
+
+let h = 1 - 2 - 3; // le calcul se fait de gauche à droite
+console.log(h);
+
+let j = 1 + 4 - 5; // sauf pour l'élévation qui se fait de droite à gauche
+console.log(j);
+
+let k = 2 ** 3 ** 2;
+console.log(k); 
+
+
+// priorité des calculs avec des parentheses
+
+let l = (1 - 2) * 3;
+console.log(l)
+
+let m = 58;
+m += 2; 
+console.log(m); 
+
+
+
+
+
+
