c75c1eca8230404ad71b6201c2c2c79e0e6a913e
Bicson STG <bicson.saint-germain@colombbus.org>
2021-10-04 12:26:11 +0200

    suite JS cours

--- 02_variables_constantes/02_05_concaténation.js
+++ 02_variables_constantes/02_05_concaténation.js
@@ -1,2 +1,34 @@
 // FEUILLE JAVASCRIPT - CONCATENATION - SEMA 
 
+
+let monAge = 18 + 2; // Une variable dans laquelle on met notre age avec un calcul.
+console.log(monAge);
+
+let phrase = 'bonjour j\'ai'; // Utilisation du caractère d'échappement.
+console.log(phrase);
+
+console.log(monAge, phrase);
+
+// Exo - Faire une alerte et mettre dans l'alerte "bonjour j'ai 20 ans"; 
+
+alert(phrase + ' ' + monAge + ' ans.');
+
+let x = 501;
+let y = 99; 
+
+// Exo - écrire dans une alerte "x contient 501, y contient 99 : leur somme est égale à 600".
+alert('x contient ' + x + ' y contient ' + y + ' leur somme est égale à ' + (x + y));
+
+// En littéraux de gabarits avec l'accent grave.
+alert(`x contient ${x}`);
+alert(`y contient ${y}`);
+alert(`x contient ${x}, y contient ${y}, leur somme est égale à ${x+y}`);
+
+
+
+
+
+
+
+
+
