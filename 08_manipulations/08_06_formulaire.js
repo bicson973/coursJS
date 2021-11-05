c6c7806347060ac527cb68b55ddd17eb17d3d2b4
Bicson STG <bicson.saint-germain@colombbus.org>
2021-11-05 15:03:45 +0100

    suite JS

--- 08_manipulations/08_06_formulaire.js
+++ 08_manipulations/08_06_formulaire.js
@@ -20,6 +20,7 @@ function controleForm() {
     }
     // On verifie la longueur des contenus du formulaire de la partie pseudo. 
 }
+// FIN PARTIE FONCTION 1 
 
 // CONTROLE DU FORMULAIRE D'INSCRIPTION AVEC UNE FONCTION :
 
@@ -37,8 +38,90 @@ function inscriptionForm(event) {
     nom = event.target.nom.value;
     email = event.target.email.value;
     password2 = event.target.password2.value;
-    repassword2 = event.target.repassword2.value;
-    console.log(prenom, nom, email, password2, repasseword2);
+    confpassword2 = event.target.confpassword2.value;
+    console.log(prenom, nom, email, password2);
     // On cible dans des variables les values de tous les champs du formulaire
+
+    // expression régulière : https://regex101.com/ 
+    let regex = /([a-z0-9](\.?|\_|\-))*@([a-z0-9]{2,})(\.[a-z]{2,}){1,}/;
+    // let regex = new RegExp("([a-z0-9](\.?|\_|\-))*@([a-z0-9]{2,})(\.[a-z]{2,}){1,}");
+    console.log(typeof regex, regex)
+
+    if (prenom !== '' || nom !== '') {
+        if (prenom.length < 2) {
+            event.target.prenom.classList.add("error");
+            // On ajoute une classe à l'élément event.target
+            // classList représente toute les classes de l'élément 
+            event.target.prenom.classList.remove("valide");
+            // On enlève la classe 
+        } else {
+            event.target.prenom.classList.add("valide");
+            event.target.prenom.classList.remove("error");
+        }
+
+        if (nom.length < 4) {
+            event.target.nom.classList.add("error");
+            event.target.nom.classList.remove("valide");
+        } else {
+            event.target.nom.classList.add("valide");
+            event.target.nom.classList.remove("error");
+        }
+    }
+
+    // CONDITION 2 L'UTILISATEUR A-T-IL SAISI LE MÊME MOT DE PASSE ?
+
+        if (confpassword2 !== password2) { /* si les mot de passe sont différents est-ce vrai ?*/
+            event.target.password2.classList.add("error");
+            event.target.password2.classList.remove("valide");
+            event.target.confpassword2.classList.add("error");
+            event.target.confpassword2.classList.remove("valide");
+            document.getElementById('label2').innerHTML = "Mots de passe pas identiques"; 
+            document.getElementById('label2').classList.add("rouge"); 
+            document.getElementById('label2').innerHTML = "Mots de passe pas identiques"; 
+            document.getElementById('label2').classList.add("rouge"); 
+        } else if(password2.length < 8 || password2.length > 12) { /* sinon si le mot de passe est trop court ou trop long */
+            event.target.password2.classList.add("error");
+            event.target.password2.classList.remove("valide"); 
+            event.target.password2.classList.add("error");
+            event.target.password2.classList.remove("valide");
+        } else { /* si les 2 conditions sont vrai tout est bon */
+            event.target.password2.classList.add("valide"); 
+            event.target.password2.classList.remove("error");
+            event.target.confpassword2.classList.add("valide");
+            event.target.confpassword2.classList.remove("error");
+        }
+
+        // Utilisation de l'expression régulière.
+        console.info(regex.test(email));
+
+        if (regex.test(email) === false) {
+            event.target.email.classList.add("error");
+            event.target.email.classList.remove("valide");
+
+        } else {
+            event.target.email.classList.remove("error");
+            event.target.email.classList.add("valide");
+        }
 }
+// FIN PARTIE FONCTION 2 
+
+    // UNE FONCTION SUR LE CLIQUE DU BOUTON EFFACER POUR VIDER LES INPUTS.
+        document.getElementById('reset').addEventListener('click', function(event){
+        event.target.form.prenom.value = '';
+        event.target.form.nom.value = '';
+        event.target.form.email.value = '';
+        event.target.form.password2.value = '';
+        event.target.form.confpassword2.value = '';
+})
+
+
+
+
+
+
+
+
+
+
+
 
